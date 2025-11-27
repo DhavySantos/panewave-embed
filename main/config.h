5f7ab48ebaf8ed61eeb8513f51ff7a625174cf92
Dhavy Santos <dhavyzhn@gmail.com>
2025-11-27 10:42:22 -0300

    init: config.h file to hold global vars
    - move all UART definitions to config.h

---
+++ main/config.h
@@ -0,0 +1,13 @@
+#ifndef COFING_H
+#define COFING_H
+
+#include "driver/gpio.h"
+
+#define UART_PORT UART_NUM_0
+#define UART_BAUD_RATE 115200
+#define UART_TX_PIN GPIO_NUM_1
+#define UART_RX_PIN GPIO_NUM_3
+#define UART_TX_BUF_SIZE 256
+#define UART_RX_BUF_SIZE 16
+
+#endif
